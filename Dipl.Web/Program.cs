87dc428451dd6d99d3786f34be882ac9a95c04eb
Vít Horký <vit.horky@easyidm.cz>
2023-10-14 11:52:54 +0200

    Add auth

--- Dipl.Web/Program.cs
+++ Dipl.Web/Program.cs
@@ -1,10 +1,26 @@
 using Dipl.Web.Components;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.MicrosoftAccount;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddAuthentication("Cookies")
+    .AddCookie(opt =>
+    {
+        opt.Cookie.Name = "AuthCookie";
+    })
+    .AddMicrosoftAccount(opt =>
+    {
+        opt.SignInScheme = "Cookies";
+        opt.ClientId = configuration["Microsoft:Id"]!;
+        opt.ClientSecret = configuration["Microsoft:Secret"]!;
+    });
 
 var app = builder.Build();
 
@@ -21,7 +37,26 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapGet("/login/microsoft", (string redirectUri) =>
+{
+    var props = new AuthenticationProperties
+    {
+        RedirectUri = redirectUri
+    };
+
+    return Results.Challenge(props, new[] { MicrosoftAccountDefaults.AuthenticationScheme });
+});
+
+app.MapGet("/login/logout", async (HttpContext httpContext) =>
+{
+    await httpContext.SignOutAsync();
+    return Results.Redirect("/");
+});
 
 app.Run();
